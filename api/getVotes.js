2109954ebc8ad8128bf73572f480983c0eb04726
Paul Siegrist <pajosieg@gmail.com>
2020-03-26 18:06:10 +0100

    use export in votes api

--- api/getVotes.js
+++ api/getVotes.js
@@ -1,11 +1,9 @@
-const getVotes = async () => {
+export const getVotes = async () => {
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      "Access-Control-Allow-Origin": "*"
     },
     body: '{ "votes": 99.3 }'
   };
 };
-
-module.exports.getVotes = getVotes;
