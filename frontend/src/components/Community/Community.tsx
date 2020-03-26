7f19feb1ce323b6772f736edb71144aaa75990b4
Paul Siegrist <pajosieg@gmail.com>
2020-03-26 21:33:59 +0100

    add init user on startup if logged in

--- frontend/src/components/Community/Community.tsx
+++ frontend/src/components/Community/Community.tsx
@@ -1,14 +1,25 @@
 import * as React from "react";
 import { withAuthenticator } from "aws-amplify-react"; // or 'aws-amplify-react-native';
-import Amplify, { Hub } from "aws-amplify";
-import aws_settings from "../../aws_settings";
+import Amplify, { Auth, Hub } from "aws-amplify";
 import Authentication from "../../Stores/Authentication";
+import aws_settings from "../../aws_settings";
 
 Amplify.configure(aws_settings);
 
 // You can get the current config object
 // const currentConfig = Auth.configure(null);
 // console.log(currentConfig);
+
+Auth.currentAuthenticatedUser({
+  bypassCache: false // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+})
+  .then(user => {
+    console.log("auth user:", user);
+    Authentication.authenticate(user);
+  })
+  .catch(err => {
+    Authentication.logout();
+  });
 
 const authenticationListener = (data: any) => {
   switch (data.payload.event) {
