a193989cbf7fd929af16fe720fe7e223b404cc76
Babette Landmesser <hey@babettelandmesser.de>
2020-04-05 18:06:37 +0200

    adds icon component and uses it

--- frontend/src/components/CategoryFilter/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter/CategoryFilter.tsx
@@ -165,6 +165,7 @@ export const CategoryFilter = ({ onFilterChange }: ICategoryFilterProps) => {
               name={name}
               checked={checked ?? false}
               onCheckboxChange={handleCategoriesChange}
+              showIcon={true}
             />
           ))}
         </div>
---
+++ frontend/src/components/Icon/Icon.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+
+import { ReactComponent as Jugend } from '../../assets/icons/jugend.svg';
+import { ReactComponent as Gottesdienst } from '../../assets/icons/gottesdienst.svg';
+import { ReactComponent as Musik } from '../../assets/icons/musik.svg';
+import { ReactComponent as Gebet } from '../../assets/icons/gebet.svg';
+import { ReactComponent as Senioren } from '../../assets/icons/senioren.svg';
+import { ReactComponent as Kurse } from '../../assets/icons/kurse.svg';
+import { ReactComponent as Konzert } from '../../assets/icons/konzert.svg';
+import { ReactComponent as Sonstiges } from '../../assets/icons/sonstiges.svg';
+
+export interface IconProps {
+  name: string;
+}
+
+export const Icon = ({name}: IconProps) => {
+
+  const iconSelect = (name: string) => {
+    switch (name.toLowerCase()) {
+      case 'jugend':
+        return <Jugend/>;
+      case 'gottesdienst':
+        return <Gottesdienst/>;
+      case 'musik':
+        return <Musik/>;
+      case 'gebet':
+        return <Gebet/>;
+      case 'senioren':
+        return <Senioren/>;
+      case 'kurse':
+      case 'kurs':
+        return <Kurse/>;
+      case 'konzert':
+        return <Konzert/>;
+      case 'sonstiges':
+      default:
+        return <Sonstiges/>;
+    }
+  };
+
+  return (
+    <>
+      {iconSelect(name)}
+    </>
+  );
+};
