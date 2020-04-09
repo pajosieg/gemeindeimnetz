8482d5e6ac23b3dea91430cd7bdbdc4acfadc5ac
Paul Siegrist <paul.siegrist@mediaman.de>
2020-04-09 18:35:39 +0200

    move cookie layer to top

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -6,6 +6,9 @@ import { Auth } from 'aws-amplify';
 import { ReactComponent as LogoutIcon } from '../../assets/icons/logout.svg';
 import { ReactComponent as LoginIcon } from '../../assets/icons/login.svg';
 import { ReactComponent as Logo } from '../../assets/svg/logo.svg';
+import CookieConsent from 'react-cookie-consent';
+import colors from '../../scss/_colors.scss';
+import { Button } from '../Button/Button';
 
 export const Header = withRouter(({ history }) => {
   const [authenticated, setAuthenticated] = useState(false);
@@ -26,49 +29,71 @@ export const Header = withRouter(({ history }) => {
   };
 
   return (
-    <div className="header">
-      <div className="header__slogan">
-        <Link to={'/'}>
-          <Logo />
-          Gemeinde im Netz
-        </Link>
+    <>
+      <div className="header">
+        <div className="header__slogan">
+          <Link to={'/'}>
+            <Logo />
+            Gemeinde im Netz
+          </Link>
+        </div>
+        <div className="header__login">
+          {authenticated && user ? (
+            <>
+              <NavLink
+                to={'/'}
+                activeClassName={'is-active'}
+                exact={true}
+                title={'Startseite'}
+                className="nav__link"
+              >
+                Startseite
+              </NavLink>{' '}
+              |{' '}
+              <NavLink
+                to={'/community'}
+                activeClassName={'is-active'}
+                title={'Meine Gemeinde'}
+                className="nav__link"
+              >
+                Meine Gemeinde
+              </NavLink>{' '}
+              |
+              <button onClick={logout} title={'Logout'}>
+                Logout
+                <LogoutIcon />
+              </button>
+            </>
+          ) : (
+            <span>
+              <Link to="/community">
+                Login
+                <LoginIcon />
+              </Link>
+            </span>
+          )}
+        </div>
+        <CookieConsent
+          onDecline={() => null}
+          location="none"
+          buttonText="Accept"
+          cookieName="cookieLayer"
+          style={{
+            position: 'absolute',
+            bottom: '0',
+            transform: 'translate(0, 100%)',
+            backgroundColor: 'rgba(80, 80, 80, 0.9',
+            color: colors.white,
+          }}
+          disableButtonStyles={true}
+          buttonStyle={{ margin: '10px 30px 10px' }}
+          ButtonComponent={Button}
+          expires={150}
+        >
+          This website uses cookies to enhance the user experience. You have to
+          accept all cookies to use this website.
+        </CookieConsent>
       </div>
-      <div className="header__login">
-        {authenticated && user ? (
-          <>
-            <NavLink
-              to={'/'}
-              activeClassName={'is-active'}
-              exact={true}
-              title={'Startseite'}
-              className="nav__link"
-            >
-              Startseite
-            </NavLink>{' '}
-            |{' '}
-            <NavLink
-              to={'/community'}
-              activeClassName={'is-active'}
-              title={'Meine Gemeinde'}
-              className="nav__link"
-            >
-              Meine Gemeinde
-            </NavLink>{' '}
-            |
-            <button onClick={logout} title={'Logout'}>
-              Logout
-              <LogoutIcon />
-            </button>
-          </>
-        ) : (
-          <span>
-            <Link to="/community">
-              Login
-              <LoginIcon />
-            </Link>
-          </span>
-        )}
-      </div>
-    </div>
+    </>
   );
 });
